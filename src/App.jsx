20bfcebbe061de0fdda19b2e35ea535972e87a08
oalejandroelias <oalejandroelias@gmail.com>
2022-06-01 00:06:15 -0300

    Base

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout'
+import EditarCliente from './paginas/EditarCliente'
+import Inicio from './paginas/Inicio'
+import NuevoCliente from './paginas/NuevoCliente'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <BrowserRouter>
+   <Routes>
+       
+     <Route path='/clientes' element={<Layout/>}>
+        <Route index element={<Inicio/>} />
+        <Route path='nuevo' element={<NuevoCliente/>} />
+        <Route path ='editar' element={<EditarCliente/>} />
+
+     </Route>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditarCliente() {
+  return (
+    <div>EditarCliente</div>
+  )
+}
+
+export default EditarCliente
